b29a8c3873d885d7de1218765b369efc11b88e20
jonascll <32561284+jonascll@users.noreply.github.com>
2020-08-28 03:10:30 +0200

    remade repository

---
+++ bot.js
@@ -0,0 +1,164 @@
+const tmi = require('tmi.js');
+const express = require('express');
+const router = express.Router()
+
+var count = 0
+var mediaRequests = []
+//const hostname = 'https://software-engineering-devops-case.azurewebsites.net';
+
+
+app = express();
+
+
+
+//Everytime you change the value, send it to all the clients connected using:
+
+
+app.use('/', router);
+
+app.use(express.static('public'));
+
+app.get("/requests", function(req,res) {
+  res.send(mediaRequests)
+})
+
+app.listen(process.env.PORT)
+
+const opts = {
+    identity: {
+      username: process.env.BOT_USERNAME,
+      password: process.env.OAUTH_TOKEN
+    },
+    channels: [
+      process.env.CHANNEL_NAME
+    ]
+  };
+
+
+  
+ 
+
+ const client = new tmi.client(opts);
+
+  // Register our event handlers (defined below)
+  client.on('message', onMessageHandler);
+
+  // Connect to Twitch:
+  client.connect();
+ 
+  // Called every time a message comes in
+  function onMessageHandler (target, context, msg, self) {
+    if (self) { return; } // Ignore messages from the bot
+    // Remove whitespace from chat message
+   
+    const commandName = msg.trim();
+    if(commandName == "#sus"){
+      var random = Math.floor(Math.random()*101)
+        if(context.username == "mrjiroxd" || context.username == "itstraptor") {
+          client.say(target, `@${context.username} ` + `You are 100% SUS`)
+        } else {
+          client.say(target, `@${context.username} ` + `You are ${random}% SUS`)
+        }
+    } 
+    if(commandName == "#gd") {
+      count ++
+      client.say(target, `Vinny said goddamit ${count} times`);
+    }
+
+    if(commandName.startsWith("#mr")) {
+      var link = commandName.substring(4)
+      if(link.startsWith("https://www.youtube.com/watch?v=") || link.startsWith("https://m.youtube.com/watch?v=") ||  link.startsWith("https://youtu.be/")) {
+        var exists = linkAlreadyExists(link)
+        if(exists) {
+          client.say(target, `@${context.username} ` + `your video is already in the queue`)
+        } else {
+          mediaRequests.push(link)
+          client.say(target, `@${context.username} ` + `your video has been added to the queue`)
+        }
+      } else {
+        client.say(target, `@${context.username} ` + `your video link is not in youtube format you naughty boy`)
+      }
+    
+    }
+   
+    if(commandName.startsWith("#") && !commandName.startsWith("#mr") && commandName != "#gd" && commandName != "#sus"){
+      client.say(
+      target,`@${context.username} ` +  `Unknown command: ` + commandName + ` => Please use a different command. available commands: #gd , !discord , #sus`);
+    } 
+  }
+
+  function linkAlreadyExists(link) {
+    if(link.startsWith("https://www.youtube.com/watch?v=")) {
+      var watchV = link.substring(link.indexOf("/watch?v=") + 9)
+      for(ytLink of mediaRequests) {
+        if(ytLink.startsWith("https://www.youtube.com/watch?v=")){
+          var existingWatchV = ytLink.substring(ytLink.indexOf("/watch?v=") + 9)
+          if (watchV == existingWatchV) {
+            return true
+          }
+        }
+        if(ytLink.startsWith("https://m.youtube.com/watch?v=")) {
+          var existingWatchV = ytLink.substring(ytLink.indexOf("/watch?v=") + 9)
+          if (watchV == existingWatchV) {
+            return true
+          }
+        }
+        if(ytLink.startsWith("https://youtu.be/")) {
+          var existingWatchV = ytLink.substring(ytLink.indexOf("youtu.be/") + 9)
+          if (watchV == existingWatchV) {
+            return true
+          }
+        }
+      }
+    }
+    if(link.startsWith("https://m.youtube.com/watch?v=")) {
+      var watchV = link.substring(link.indexOf("/watch?v=") + 9)
+      for(ytLink of mediaRequests) {
+        if(ytLink.startsWith("https://www.youtube.com/watch?v=")){
+          var existingWatchV = ytLink.substring(ytLink.indexOf("/watch?v=") + 9)
+          if (watchV == existingWatchV) {
+            return true
+          }
+        }
+        if(ytLink.startsWith("https://m.youtube.com/watch?v=")) {
+          var existingWatchV = ytLink.substring(ytLink.indexOf("/watch?v=") + 9)
+          if (watchV == existingWatchV) {
+            return true
+          }
+        }
+        if(ytLink.startsWith("https://youtu.be/")) {
+          var existingWatchV = ytLink.substring(ytLink.indexOf("youtu.be/") + 9)
+          if (watchV == existingWatchV) {
+            return true
+          }
+        }
+      }
+    }
+    if(link.startsWith("https://youtu.be/")) {
+      var watchV = link.substring(link.indexOf("youtu.be/") + 9)
+      for(ytLink of mediaRequests) {
+        if(ytLink.startsWith("https://www.youtube.com/watch?v=")){
+          var existingWatchV = ytLink.substring(ytLink.indexOf("/watch?v=") + 9)
+          if (watchV == existingWatchV) {
+            return true
+          }
+        }
+        if(ytLink.startsWith("https://m.youtube.com/watch?v=")) {
+          var existingWatchV = ytLink.substring(ytLink.indexOf("/watch?v=") + 9)
+          if (watchV == existingWatchV) {
+            return true
+          }
+        }
+        if(ytLink.startsWith("https://youtu.be/")) {
+          var existingWatchV = ytLink.substring(ytLink.indexOf("youtu.be/") + 9)
+          if (watchV == existingWatchV) {
+            return true
+          }
+        }
+      }
+    }
+    return false
+  }
+
+
+
---
+++ public/youtubequeue.js
@@ -0,0 +1,82 @@
+const url = "https://vmengabot.herokuapp.com/requests"
+var mediaRequests = []
+var currentVideoIndex = 0
+setInterval(() => {
+      httpGet(url)
+      var iframe = document.getElementById('ytplayer')
+      if(Array.isArray(mediaRequests) && mediaRequests.length && iframe.getAttribute('src') == "") {
+        iframe.src = parseYoutubeLink(mediaRequests[0])
+      }
+}, 500);
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+  document.getElementById("nextButton").addEventListener("click", nextVideoClick)
+  var iframe = document.getElementById('ytplayer')
+  httpGet(url)
+  });
+  function httpGet(theUrl)
+  {
+      var xmlHttp = new XMLHttpRequest();
+      xmlHttp.open( "GET", theUrl, false );
+      xmlHttp.send( null );
+      var gottenRequests = JSON.parse(xmlHttp.responseText)
+      if(!checkIfArraysAreSame(mediaRequests, gottenRequests)) {
+        mediaRequests = JSON.parse(xmlHttp.responseText)
+        emptyTable()
+        mediaRequests.forEach(setTableRows)
+      }
+  }
+
+  function setTableRows(item, index) {
+    var table = document.getElementById("linkTable")
+    var row = table.insertRow(index + 1)
+    var dataInRow = row.insertCell(0)
+    dataInRow.innerHTML = `<a href='${item}'>${item}</a>`
+  }
+
+  function checkIfArraysAreSame(array1, array2) {
+    if(array1.length !== array2.length) {return false}
+    for(var i = 0 ; array1.length > i; i++) {
+      if(array1[i] !== array2[i]) {
+        return false
+      }
+    }
+      return true
+  }
+
+  function emptyTable() {
+    var table = document.getElementById("linkTable")
+    while(table.hasChildNodes()) {
+      table.removeChild(table.firstChild)
+    }
+    var row = table.insertRow(0)
+    var th = document.createElement("th")
+    th.innerHTML = "Youtube links"
+    row.appendChild(th)
+  }
+
+  function nextVideoClick(){
+      if(mediaRequests.length <= 1)  {
+        window.alert("there is none or only 1 video")
+      } else {
+        var iframe = document.getElementById('ytplayer')
+        iframe.src = parseYoutubeLink(mediaRequests[++currentVideoIndex])
+      }
+  }
+
+  function parseYoutubeLink(link) {
+
+    const partEmbed = "https://www.youtube.com/embed/"
+    if(link.startsWith("https://www.youtube.com/watch?v=")) {
+      var watchV = link.substring(link.indexOf("/watch?v=") + 9)
+      return youtubeLink = partEmbed + watchV
+    }
+    if(link.startsWith("https://m.youtube.com/watch?v=")) {
+      var watchV = link.substring(link.indexOf("/watch?v=") + 9)
+      return youtubeLink = partEmbed + watchV
+    }
+    if(link.startsWith("https://youtu.be/")) {
+      var watchV = link.substring(link.indexOf("youtu.be/") + 9)
+      return youtubeLink = partEmbed + watchV
+    }
+  }
